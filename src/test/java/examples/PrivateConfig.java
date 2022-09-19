9044a597ce8d7cf451a821ece9efc2f9b18b7be5
chronos-labs-sj <shikai@satori.finance>
2022-09-19 17:20:34 -0400

    change config file to read from sys env vars

--- src/test/java/examples/PrivateConfig.java
+++ src/test/java/examples/PrivateConfig.java
@@ -4,10 +4,10 @@ public final class PrivateConfig {
     private PrivateConfig() {
     }
 
-    public static final String API_KEY = "";
-    public static final String SECRET_KEY = "";
+    public static final String API_KEY = System.getenv("BINANCE_API_KEY");
+    public static final String SECRET_KEY = System.getenv("BINANCE_SECRET_KEY");
     public static final String BASE_URL = "https://testnet.binance.vision";
-    public static final String TESTNET_API_KEY = "";
-    public static final String TESTNET_SECRET_KEY = "";
+    public static final String TESTNET_API_KEY = System.getenv("BINANCE_TESTNET_API_KEY");
+    public static final String TESTNET_SECRET_KEY = System.getenv("BINANCE_TESTNET_SECRET_KEY");
 
 }
