2c15f0833a913d510afb286391aae1d069654c4a
chronos-labs-sj <shikai@satori.finance>
2022-12-17 17:48:52 -0500

    add PrivateConfig for src

---
+++ src/main/java/com/binance/connector/client/PrivateConfig.java
@@ -0,0 +1,9 @@
+package com.binance.connector.client;
+
+public class PrivateConfig {
+    public static final String API_KEY = System.getenv("BINANCE_API_KEY");
+    public static final String SECRET_KEY = System.getenv("BINANCE_SECRET_KEY");
+    public static final String BASE_URL = "https://testnet.binance.vision";
+    public static final String TESTNET_API_KEY = System.getenv("BINANCE_TESTNET_API_KEY");
+    public static final String TESTNET_SECRET_KEY = System.getenv("BINANCE_TESTNET_SECRET_KEY");
+}
