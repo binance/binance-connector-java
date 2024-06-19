68e6251539bf0824de291ae9530e337f2c193f54
alplabin <122352306+alplabin@users.noreply.github.com>
2024-06-19 18:03:51 +0900

    3.3.0 release

--- src/main/java/com/binance/connector/client/enums/DefaultUrls.java
+++ src/main/java/com/binance/connector/client/enums/DefaultUrls.java
@@ -5,8 +5,8 @@ public final class DefaultUrls {
     public static final String WS_URL = "wss://stream.binance.com:9443";
     public static final String WS_API_URL = "wss://ws-api.binance.com:443/ws-api/v3";
     public static final String TESTNET_URL = "https://testnet.binance.vision";
-    public static final String TESTNET_WS_URL = "wss://testnet.binance.vision";
-    public static final String TESTNET_WS_API_URL = "wss://testnet.binance.vision/ws-api/v3";
+    public static final String TESTNET_WS_URL = "wss://stream.testnet.binance.vision";
+    public static final String TESTNET_WS_API_URL = "wss://ws-api.testnet.binance.vision/ws-api/v3";
     public static final String MARKET_URL = "https://data-api.binance.vision";
     public static final String MARKET_WS_URL = "wss://data-stream.binance.vision";
     private DefaultUrls() {
